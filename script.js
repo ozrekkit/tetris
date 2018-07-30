c57f07c932f08dff3a63cd413267f91c66ee5e88
OlehZ <oleg.zago@gmail.com>
2018-07-30 23:46:17 +0200

    add function playerDrop

--- script.js
+++ script.js
@@ -28,6 +28,11 @@ function drawMatrix(matrix, offset) {
     });
 };
 
+function playerDrop() {
+    player.pos.y++;
+    dropCounter = 0;
+}
+
 let dropCounter = 0;
 let dropInterval = 1000;
 
@@ -37,8 +42,7 @@ function update(time = 0) {
     lastTime = time;
     dropCounter += deltaTime;
     if (dropCounter > dropInterval) {
-        player.pos.y++;
-        dropCounter = 0;
+       playerDrop();
     };
     draw();
     requestAnimationFrame(update);
@@ -54,8 +58,11 @@ document.addEventListener('keydown', event => {
     if (event.keyCode === 37) {
         player.pos.x--;
     }
-    if (event.keyCode === 39) {
+    else if (event.keyCode === 39) {
         player.pos.x++;
+    }
+    else if (event.keyCode === 40) {
+        playerDrop();
     }
 })
 
